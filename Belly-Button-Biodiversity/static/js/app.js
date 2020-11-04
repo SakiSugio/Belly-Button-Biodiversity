2671f401efd6a8048ce8c8533690012dcd88f1a3
Saki Sugio <sakisugio9@gmail.com>
2020-11-03 18:51:44 -0600

    working on bubble

--- Belly-Button-Biodiversity/static/js/app.js
+++ Belly-Button-Biodiversity/static/js/app.js
@@ -34,6 +34,7 @@ function demographic (userId) {
 function optionChanged(userId) {
     demographic(userId)
     barChart(userId)
+    bubbleData(userId)
 }
 
 function barChart (userId) {
@@ -42,15 +43,15 @@ function barChart (userId) {
         var filterData = samplesData.samples.filter(samples => samples.id == userId)
         var firstSample = filterData[0]
         console.log(firstSample)
-    var xaxis = firstSample.sample_values.slice(0,10).reverse()
-    var yaxis = firstSample.otu_ids.map(row => `OTUid ${row}`).slice(0,10).reverse()
-    var text = firstSample.otu_labels.slice(0,10).reverse()
+    var sampleValues = firstSample.sample_values.slice(0,10).reverse()
+    var otuIds = firstSample.otu_ids.map(row => `OTU ID ${row}`).slice(0,10).reverse()
+    var otuLabels = firstSample.otu_labels.slice(0,10).reverse()
 
     // Trace1 for the sample_values data
     var trace1 = {
-        x: xaxis,
-        y: yaxis,
-        text: text,
+        x: sampleValues,
+        y: otuIds,
+        text: otuLabels,
         type: "bar", 
         orientation: "h"
     };
@@ -60,6 +61,23 @@ function barChart (userId) {
 
     // Render the plot to the div tag with id "bar"
     Plotly.newPlot("bar", chartData);
+
+
+    // Babble chart
+    var trace2 = {
+        x: otuIds,
+        y: sampleValues,
+        text: otuLabels,
+        mode: "makers",
+        marker: {
+            color: otuIds,
+            size: sampleValues
+        }
+    };
+
+    var bubbleData = [trace2];
+
+    Plotly.newPlot("bubble", bubbleData);
 })};
 
 
